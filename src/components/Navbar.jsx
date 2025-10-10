dfddf5a3aedfbc357df00433cdeb7eb13ee3b1f7
Elkana Maina <elisoft.engineer@gmail.com>
2025-10-10 12:28:17 +0300

    Wrap with navbar container

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,26 @@ import { NavLink } from "react-router-dom";
 import ThemeToggle from "./ThemeToggler";
 
 const Navbar = () => {
-  return (<nav class="navbar">
-      <div class="brand">
-        <a class="name" href="index.html">
-          <span class="first">Elkana</span>
-          <span class="last">Maina</span>
-        </a>
-      </div>
-      <div class="nav-links">
-        <NavLink className="link" to="/">Home</NavLink>
-        <NavLink className="link" to="/projects">Projects</NavLink>
-        <NavLink className="link" to="/experience">Experience</NavLink>
-        <NavLink className="link" to="/skills">Skills</NavLink>
-        <NavLink className="link" to="/certificates">Certificates</NavLink>
-        <ThemeToggle className="theme-toggler" />
-      </div>
-    </nav>)
+  return (
+    <div className="navbar-container">
+      <nav class="navbar">
+        <div class="brand">
+          <a class="name" href="index.html">
+            <span class="first">Elkana</span>
+            <span class="last">Maina</span>
+          </a>
+        </div>
+        <div class="nav-links">
+          <NavLink className="link" to="/">Home</NavLink>
+          <NavLink className="link" to="/projects">Projects</NavLink>
+          <NavLink className="link" to="/experience">Experience</NavLink>
+          <NavLink className="link" to="/skills">Skills</NavLink>
+          <NavLink className="link" to="/certificates">Certificates</NavLink>
+          <ThemeToggle className="theme-toggler" />
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
