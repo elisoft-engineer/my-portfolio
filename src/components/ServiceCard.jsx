6275d85464a1ac0b4d392d13596b63933dd6319b
Elkana Maina <elisoft.engineer@gmail.com>
2025-10-22 18:07:15 +0300

    Initial Commit

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,25 @@
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const ServiceCard = ({index, icon, title, description }) => {
+	return (
+		<Tilt className="service-card">
+			<motion.div>
+				<article
+					className="card"
+					key={index}
+					options={{max: 45, scale: 1, speed: 450}}
+				>
+					<div className="icon">
+						<FontAwesomeIcon icon={icon} className="icon large" />
+					</div>
+					<h3 className="title">{title}</h3>
+					<p className="description">{description}</p>
+				</article>
+			</motion.div>
+		</Tilt>
+	);
+}
+
+export default ServiceCard;
