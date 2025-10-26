9c3b7932fd3686bde65c4ebecae5c440d776dd7a
Elkana Maina <elisoft.engineer@gmail.com>
2025-10-26 16:27:41 +0300

    Include the Navbar and Footer in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home/>}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
