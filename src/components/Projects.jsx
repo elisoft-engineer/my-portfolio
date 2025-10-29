07c93585678a921215a774941144ed05de740a00
Elkana Maina <elisoft.engineer@gmail.com>
2025-10-29 11:23:25 +0300

    Add project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons/faArrowUpRightFromSquare';
+
 import { projects } from '../constants'
+
 
 const ProjectsPage = () => {
 	return (
-		
+		<section className="projects-section">
+			{projects.map((project, index) => {
+				return <div className="card" key={index}>
+					<div className="image">
+						<img src={project.image} alt={project.title + " image"} />
+					</div>
+					<div className="meta">
+						<h2 className="title">{project.title}</h2>
+						<p className="description">{project.description}</p>
+						<div className="skills">
+							{project.technologies.map((skill, index) => {
+								return <span className="skill" key={index}>{skill}</span>
+							})}
+							
+						</div>
+						<div className="links">
+							<a href="#" target="_blank" className="link">
+								<FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" />
+								Visit Website
+							</a>
+						</div>
+					</div>
+				</div>
+			})}
+		</section>
 	);
 }
 
