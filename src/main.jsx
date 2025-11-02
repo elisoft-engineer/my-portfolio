1f410e849b675309ea280bc9f29ebc009ef45a3f
Elkana Maina <elisoft.engineer@gmail.com>
2025-11-02 12:23:37 +0300

    Add DialogProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ThemeProvider from './context/ThemeContext.jsx'
+import { DialogProvider } from "./context/DialogContext";
+import Dialog from "./components/Dialog";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <DialogProvider>
+        <App />
+        <Dialog />
+      </DialogProvider>
     </ThemeProvider>
   </StrictMode>,
 )
