4b22dda76b67fcb5dac2efeca43622c4b670f2d7
Elkana Maina <elisoft.engineer@gmail.com>
2025-11-23 12:03:31 +0300

    Add dropdown menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect, useRef } from "react";
+
 import { NavLink } from "react-router-dom";
 import ThemeToggle from "./ThemeToggler";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const dropdownRef = useRef(null);
+  const togglerRef = useRef(null);
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        togglerRef.current &&
+        !togglerRef.current.contains(event.target)
+      ) {
+        setIsCollapsed(true); // collapse menu
+      }
+    };
+
+    if (!isCollapsed) {
+      document.addEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isCollapsed]);
+
   return (
     <div className="navbar-container">
       <nav className="navbar">
@@ -14,6 +43,16 @@ const Navbar = () => {
           <NavLink className="link" to="/projects">Projects</NavLink>
           <NavLink className="link" to="/skills">Skills</NavLink>
           <NavLink className="link" to="/certifications">Certificates</NavLink>
+          <ThemeToggle className="theme-toggler" />
+        </div>
+        <div ref={togglerRef} id="nav-toggler" className="nav-toggler" onClick={() => setIsCollapsed(!isCollapsed)}>
+          <FontAwesomeIcon icon={isCollapsed ? faBars : faTimes } className="icon" />
+        </div>
+        <div ref={dropdownRef} className={`dropdown-menu ${!isCollapsed ? "open" : ""}`}>
+          <NavLink className="link" to="/" onClick={() => setIsCollapsed(true)}>Home</NavLink>
+          <NavLink className="link" to="/projects" onClick={() => setIsCollapsed(true)}>Projects</NavLink>
+          <NavLink className="link" to="/skills" onClick={() => setIsCollapsed(true)}>Skills</NavLink>
+          <NavLink className="link" to="/certifications" onClick={() => setIsCollapsed(true)}>Certificates</NavLink>
           <ThemeToggle className="theme-toggler" />
         </div>
       </nav>
