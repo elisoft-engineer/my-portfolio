67f6a693f26133fc8f0a62e73aae676bfe1d1b42
Elkana Maina <elisoft.engineer@gmail.com>
2026-01-21 17:45:30 +0300

    Convert the project to Next.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ThemeProvider from './context/ThemeContext.jsx'
-import { DialogProvider } from "./context/DialogContext";
-import Dialog from "./components/Dialog";
+import { DialogProvider } from "./context/DialogContext.jsx";
+import Dialog from "./components/Dialog.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
