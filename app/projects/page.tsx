c258912de3a2e24e92e3a3f19526e5426a303fef
Elkana Maina <elisoft.engineer@gmail.com>
2026-01-26 12:36:41 +0300

    Initial Commit

---
+++ app/projects/page.tsx
@@ -0,0 +1,35 @@
+import { projects } from '../constants';
+import styles from './page.module.css';
+import Image from 'next/image';
+import { ExternalLink } from 'lucide-react';
+
+
+const Projects = () => {
+  return (
+    <section className={styles.projectSection}>
+      {projects.map((project, index) => {
+        return <div className={styles.card} key={index}>
+          <Image src={project.image} width={610} alt={project.title + " image"} />
+          <div className={styles.meta}>
+            <h2 className={styles.title}>{project.title}</h2>
+            <p className={styles.description}>{project.description}</p>
+            <div className={styles.skills}>
+              {project.technologies.map((skill, index) => {
+                return <span className={styles.skill} key={index}>{skill}</span>
+              })}
+              
+            </div>
+            <div className={styles.links}>
+              <a href={project.link ?? "#"} target="_blank" className={styles.link}>
+                <ExternalLink size={20} />
+                Visit Website
+              </a>
+            </div>
+          </div>
+        </div>
+      })}
+    </section>
+  );
+}
+
+export default Projects;
