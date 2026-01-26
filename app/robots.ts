b6b4d6d5a78019f530e9d918ba3748f7f5f70315
Elkana Maina <elisoft.engineer@gmail.com>
2026-01-26 15:33:25 +0300

    Initial Commit

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'https://elkana.ellypad.com/sitemap.xml',
+  };
+}
