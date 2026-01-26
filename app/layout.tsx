811dc02d894071d085bf514017fa1bd59cd887bf
Elkana Maina <elisoft.engineer@gmail.com>
2026-01-26 12:00:37 +0300

    Delete the inter tight font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import {Inter_Tight} from "next/font/google";
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
@@ -15,12 +14,6 @@ export const metadata: Metadata = {
   description: "A software project management platform",
 };
 
-const interTight = Inter_Tight({
-  subsets: ['latin'],
-  weight: ['400', '600', '700'],
-  variable: '--font-inter-tight',
-});
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={interTight.variable}>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
           <main className="main">{children}</main>
