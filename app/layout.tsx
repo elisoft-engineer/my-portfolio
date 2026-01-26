3b77df581d0a057418f586a92920c3f1284900ac
Elkana Maina <elisoft.engineer@gmail.com>
2026-01-26 16:34:24 +0300

    Correct the og image url

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     siteName: "Elkana Maina Portfolio",
     images: [
       {
-        url: "/og-image.png",
+        url: "/opengraph-image",
         width: 1200,
         height: 630,
         alt: "Elkana Maina Portfolio",
