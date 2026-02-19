0c46b182383593a3e5714d3c91caf32e649b8fa5
Elkana Maina <elisoft.engineer@gmail.com>
2026-02-19 21:14:48 +0300

    Adjust cert provider names

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -267,89 +267,89 @@ const certifications: Certification[] = [
   {
     title: "HTML, CSS, and Javascript for Web Developers",
     institution: {
-      name: "John Hopkins University",
+      name: "John Hopkins University via Coursera",
       logo: webSkills,
     },
     skills: ["Web Development", "Front-End Development", "HTML & CSS", "JavaScript", "Ajax", "Bootstrap"],
-    obtainedOn: "October 2023",
+    obtainedOn: "Oct 2023",
     url: "https://coursera.org/verify/8NS8YVRGR5PD",
   },
   {
     title: "Django Web Framework",
     institution: {
-      name: "Meta",
+      name: "Meta via Coursera",
       logo: djangoWebFramework,
     },
     skills: ["Django", "Web Servers", "Model View Controller", "Back-End Development", "Object-Relational Mapping"],
-    obtainedOn: "January 2024",
+    obtainedOn: "Jan 2024",
     url: "https://coursera.org/verify/4QC76KYF8VXH",
   },
   {
     title: "Python Essentials 1",
     institution: {
-      name: "Cisco",
+      name: "Cisco Networking Academy",
       logo: pythonEssentials1,
     },
     skills: ["Computer Programming", "Python", "Algorithmic Thinking", "Procedural Programming"],
-    obtainedOn: "September 2023",
+    obtainedOn: "Sept 2023",
     url: "https://www.credly.com/badges/150e4072-5a5f-4be4-82a9-20fc7e607a9c/public_url",
   },
   {
     title: "Python Essentials 2",
     institution: {
-      name: "Cisco",
+      name: "Cisco Networking Academy",
       logo: pythonEssentials2,
     },
     skills: ["Python", "Object-Oriented Programming", "Generators", "Modules", "Package Management"],
-    obtainedOn: "October 2023",
+    obtainedOn: "Oct 2023",
     url: "https://www.credly.com/badges/e00863c6-3d77-47cc-8fb2-d185499b1fb1/public_url",
   },
   {
     title: "Python Basics",
     institution: {
-      name: "University of Michigan",
+      name: "University of Michigan  via Coursera",
       logo: pythonBasics,
     },
     skills: ["Python (Programming Language)", "Data Structures", "Programming Principles"],
-    obtainedOn: "April 2023",
+    obtainedOn: "Apr 2023",
     url: "https://coursera.org/verify/S8WPGT8HB3MK",
   },
   {
     title: "Applying AI Principles with Google Cloud",
     institution: {
-      name: "Google",
+      name: "Google Cloud Skills Boost",
       logo: applyingAIPrinciples,
     },
     skills: ["AI", "Generative AI", "Responsible AI Principles"],
-    obtainedOn: "September 2023",
+    obtainedOn: "Sept 2023",
     url: "https://www.skills.google/public_profiles/93e7128c-3e45-4599-b1ab-d1d28d546933/badges/4961991",
   },
   {
     title: "Introduction to Data Science",
     institution: {
-      name: "Cisco",
+      name: "Cisco Networking Academy",
       logo: introDataScience,
     },
     skills: ["Data Analysis", "Data Collection", "Data Validation"],
-    obtainedOn: "October 2024",
+    obtainedOn: "Oct 2024",
     url: "https://www.credly.com/badges/56feae9f-b2d9-4346-b4e6-ae85b22ba268/public_url",
   },
   
   {
     title: "Operating Systems Basics",
     institution: {
-      name: "Cisco",
+      name: "Cisco Networking Academy",
       logo: operatingSystems,
     },
     skills: ["Android", "Linux", "Windows", "Operating Systems Security"],
-    obtainedOn: "February 2023",
+    obtainedOn: "Feb 2023",
     url: "https://www.credly.com/badges/809c322e-fd64-4663-a0ec-48dcf7b4a105/public_url",
   },
   
   {
     title: "Object Oriented Programming in C++",
     institution: {
-      name: "University of London",
+      name: "University of London via Coursera",
       logo: cppCrypto,
     },
     skills: ["C++ (Programming Language)", "Object-Oriented Programming", "Programming Principles"],
@@ -360,41 +360,41 @@ const certifications: Certification[] = [
   {
     title: "Introduction to Generative AI",
     institution: {
-      name: "Google",
+      name: "Google Cloud Skills Boost",
       logo: introGenAI,
     },
     skills: ["AI", "Generative AI"],
-    obtainedOn: "September 2023",
+    obtainedOn: "Sept 2023",
     url: "https://www.skills.google/public_profiles/93e7128c-3e45-4599-b1ab-d1d28d546933/badges/4954381",
   },
   {
     title: "Introduction to Large Language Models",
     institution: {
-      name: "Google",
+      name: "Google Cloud Skills Boost",
       logo: introLLMs,
     },
     skills: ["AI", "Generative AI", "Large Language Models"],
-    obtainedOn: "September 2023",
+    obtainedOn: "Sept 2023",
     url: "https://www.skills.google/public_profiles/93e7128c-3e45-4599-b1ab-d1d28d546933/badges/4955141",
   },
   {
     title: "Introduction to Responsible AI",
     institution: {
-      name: "Google",
+      name: "Google Cloud Skills Boost",
       logo: introResponsibleAI,
     },
     skills: ["AI", "Generative AI", "Large Language Models"],
-    obtainedOn: "September 2023",
+    obtainedOn: "Sept 2023",
     url: "https://www.skills.google/public_profiles/93e7128c-3e45-4599-b1ab-d1d28d546933/badges/4955540",
   },
   {
     title: "Generative AI Fundamentals",
     institution: {
-      name: "Google",
+      name: "Google Cloud Skills Boost",
       logo: genAIFundamentals,
     },
     skills: ["AI", "Generative AI"],
-    obtainedOn: "September 2023",
+    obtainedOn: "Sept 2023",
     url: "https://www.skills.google/public_profiles/93e7128c-3e45-4599-b1ab-d1d28d546933/badges/4956000",
   },
   
@@ -402,41 +402,41 @@ const certifications: Certification[] = [
   {
     title: "Networking Devices and Initial Configuration",
     institution: {
-      name: "Cisco",
+      name: "Cisco Networking Academy",
       logo: netDevices,
     },
     skills: ["Cisco IOS", "ARP", "Cisco Devices", "DNS", "DHCP"],
-    obtainedOn: "January 2025",
+    obtainedOn: "Jan 2025",
     url: "https://www.credly.com/badges/140ba71b-1286-47ce-b2ff-2ce132e10bd2/public_url",
   },
   {
     title: "Networking Basics",
     institution: {
-      name: "Cisco",
+      name: "Cisco Networking Academy",
       logo: netBasics,
     },
     skills: ["Application Layer Services", "IPV4 Addresses", "Protocol Standards"],
-    obtainedOn: "March 2025",
+    obtainedOn: "Mar 2025",
     url: "https://www.credly.com/badges/4db1f5b9-41a9-4f5c-8d0e-b97a0440dc63/public_url",
   },
   {
     title: "Network Addressing and Basic Troubleshooting",
     institution: {
-      name: "Cisco",
+      name: "Cisco Networking Academy",
       logo: netAddressing,
     },
     skills: ["Network Troubleshooting", "Cisco Routers", "Cisco Switches"],
-    obtainedOn: "March 2025",
+    obtainedOn: "Mar 2025",
     url: "https://www.credly.com/badges/2e9d1c15-a68e-40b5-bff5-d6ed4c395b06/public_url",
   },
   {
     title: "Network Support Security",
     institution: {
-      name: "Cisco",
+      name: "Cisco Networking Academy",
       logo: netSupport,
     },
     skills: ["Network Troubleshooting", "Documentation", "User Support"],
-    obtainedOn: "April 2025",
+    obtainedOn: "Apr 2025",
     url: "https://www.credly.com/badges/c65f2ea0-9a93-4048-8f78-d8dbab5e40f8/public_url",
   },
 ]
