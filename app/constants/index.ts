1f17ec54204b63fcfbe4e1122f10f480b8368956
Elkana Maina <elisoft.engineer@gmail.com>
2026-07-29 13:35:13 +0300

    fix: update expertise

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,22 +1,18 @@
-import css from '@/assets/icons/css.svg';
-import dart from '@/assets/icons/dart.svg';
-import django from '@/assets/icons/django.svg';
-import docker from '@/assets/icons/docker.svg';
-import fastapi from '@/assets/icons/fastapi.svg';
-import flutter from '@/assets/icons/flutter.svg';
-import html from '@/assets/icons/html.svg';
-import javascript from '@/assets/icons/javascript.svg';
-import nextjs from '@/assets/icons/nextjs.svg';
-import nodejs from '@/assets/icons/nodejs.svg';
-import python from '@/assets/icons/python.svg';
-import react from '@/assets/icons/react.svg';
-import sqlalchemy from '@/assets/icons/sqlalchemy.svg';
-import swagger from '@/assets/icons/swagger.svg';
-import vite from '@/assets/icons/vite.svg';
-
-import falcosendImg from '@/assets/images/falcosend.webp';
-import yummypointImg from '@/assets/images/yummypoint.webp';
-import mystudynetImg from '@/assets/images/mystudynet.webp';
+import css from "@/assets/icons/css.svg";
+import dart from "@/assets/icons/dart.svg";
+import django from "@/assets/icons/django.svg";
+import docker from "@/assets/icons/docker.svg";
+import fastapi from "@/assets/icons/fastapi.svg";
+import flutter from "@/assets/icons/flutter.svg";
+import html from "@/assets/icons/html.svg";
+import javascript from "@/assets/icons/javascript.svg";
+import nextjs from "@/assets/icons/nextjs.svg";
+import nodejs from "@/assets/icons/nodejs.svg";
+import python from "@/assets/icons/python.svg";
+import react from "@/assets/icons/react.svg";
+import sqlalchemy from "@/assets/icons/sqlalchemy.svg";
+import swagger from "@/assets/icons/swagger.svg";
+import vite from "@/assets/icons/vite.svg";
 
 // coursera icons
 import cppCrypto from "@/assets/images/CPP.webp";
@@ -41,7 +37,15 @@ import operatingSystems from "@/assets/images/Operating Systems.webp";
 import pythonEssentials1 from "@/assets/images/Python Essentials 1.webp";
 import pythonEssentials2 from "@/assets/images/Python Essentials 2.webp";
 
-import { Bot, Cloud, CodeXml, LucideIcon, MonitorSmartphone, Server, Settings } from "lucide-react";
+import {
+  Bot,
+  Cloud,
+  CodeXml,
+  LucideIcon,
+  MonitorSmartphone,
+  Server,
+  Settings,
+} from "lucide-react";
 import { StaticImageData } from "next/image";
 
 interface Technology {
@@ -64,16 +68,6 @@ interface Service {
   icon: LucideIcon;
   title: string;
   description: string;
-}
-
-interface Project {
-  title: string;
-  description: string;
-  image: StaticImageData | string;
-  technologies: string[];
-  link?: string;
-  active: boolean;
-  launchingSoon?: boolean;
 }
 
 interface Certification {
@@ -148,103 +142,113 @@ const technologies: Technology[] = [
     name: "Vite",
     icon: vite,
   },
-]
+];
 
 const expertise: Expertise[] = [
   {
     name: "Programming Languages",
     icon: CodeXml,
     skills: [
-      {name: "Python", percentage: 95},
-      {name: "JavaScript", percentage: 90},
-      {name: "TypeScript", percentage: 85},
-      {name: "Dart", percentage: 90},
-      {name: "Kotlin", percentage: 85},
-      {name: "C++", percentage: 90},
-      {name: "HTML5", percentage: 95},
-      {name: "CSS3", percentage: 95},
-    ]
+      { name: "Python", percentage: 95 },
+      { name: "JavaScript", percentage: 90 },
+      { name: "TypeScript", percentage: 85 },
+      { name: "Dart", percentage: 90 },
+      { name: "Kotlin", percentage: 85 },
+      { name: "C++", percentage: 90 },
+      { name: "HTML5", percentage: 95 },
+      { name: "CSS3", percentage: 95 },
+    ],
   },
   {
     name: "Frontend Technologies",
     icon: MonitorSmartphone,
     skills: [
-      {name: "React", percentage: 90},
-      {name: "Next.js", percentage: 85},
-      {name: "Tailwind CSS", percentage: 85},
-      {name: "Flutter", percentage: 90},
-      {name: "Material UI", percentage: 90},
-    ]
+      { name: "React", percentage: 90 },
+      { name: "Next.js", percentage: 85 },
+      { name: "Tailwind CSS", percentage: 85 },
+      { name: "Flutter", percentage: 90 },
+      { name: "Material UI", percentage: 90 },
+    ],
   },
   {
     name: "Backend Technologies",
     icon: Server,
     skills: [
-      {name: "Django", percentage: 95},
-      {name: "FastAPI", percentage: 95},
-      {name: "SQLAlchemy", percentage: 90},
-      {name: "Node.js", percentage: 90},
-      {name: "Express.js", percentage: 90},
-      {name: "PostgresSQL", percentage: 90},
-      {name: "MongoDB", percentage: 85}
-    ]
+      { name: "Django", percentage: 95 },
+      { name: "FastAPI", percentage: 95 },
+      { name: "SQLAlchemy", percentage: 90 },
+      { name: "Node.js", percentage: 90 },
+      { name: "Express.js", percentage: 90 },
+      { name: "PostgresSQL", percentage: 90 },
+      { name: "MongoDB", percentage: 85 },
+    ],
   },
   {
     name: "AI & Machine Learning",
     icon: Bot,
     skills: [
-      {name: "TensorFlow", percentage: 85},
-      {name: "Keras", percentage: 85},
-      {name: "ScikitLearn", percentage: 85},
-      {name: "Pandas", percentage: 90},
-      {name: "Numpy", percentage: 90},
-    ]
+      { name: "TensorFlow", percentage: 85 },
+      { name: "Keras", percentage: 85 },
+      { name: "ScikitLearn", percentage: 85 },
+      { name: "Pandas", percentage: 90 },
+      { name: "Numpy", percentage: 90 },
+    ],
   },
   {
     name: "DevOps",
     icon: Cloud,
     skills: [
-      {name: "Git", percentage: 90},
-      {name: "Docker", percentage: 85},
-      {name: "GHCR", percentage: 85},
-      {name: "CI/CD", percentage: 85},
-      {name: "NGINX", percentage: 85},
-    ]
+      { name: "Git", percentage: 90 },
+      { name: "Docker", percentage: 85 },
+      { name: "GHCR", percentage: 85 },
+      { name: "CI/CD", percentage: 85 },
+      { name: "NGINX", percentage: 85 },
+    ],
   },
-]
+];
 
 const services: Service[] = [
   {
     icon: MonitorSmartphone,
     title: "Business Websites",
-    description: "Creating a 24/7 digital storefront for your brand, ensuring your business is accessible to a global market through fast, responsive web design."
+    description:
+      "Creating a 24/7 digital storefront for your brand, ensuring your business is accessible to a global market through fast, responsive web design.",
   },
   {
     icon: Settings,
     title: "Enterprise Software",
-    description: "Custom-built systems for inventory, billing, and workflow. I help organizations reduce human error and save time through automated dashboards."
+    description:
+      "Custom-built systems for inventory, billing, and workflow. I help organizations reduce human error and save time through automated dashboards.",
   },
   {
     icon: Server,
     title: "Backend & API Development",
-    description: "Designing the 'brain' of your application—secure, high-performance servers that ensure your data stays safe and your software runs smoothly."
+    description:
+      "Designing the 'brain' of your application—secure, high-performance servers that ensure your data stays safe and your software runs smoothly.",
   },
   {
     icon: Cloud,
     title: "Deployment & DevOps",
-    description: "Handling the technical heavy lifting of taking software live, ensuring your systems are monitored, maintained, and always available."
-  }
-]
+    description:
+      "Handling the technical heavy lifting of taking software live, ensuring your systems are monitored, maintained, and always available.",
+  },
+];
 
 const certifications: Certification[] = [
-  
   {
     title: "HTML, CSS, and Javascript for Web Developers",
     institution: {
       name: "John Hopkins University via Coursera",
       logo: webSkills,
     },
-    skills: ["Web Development", "Front-End Development", "HTML & CSS", "JavaScript", "Ajax", "Bootstrap"],
+    skills: [
+      "Web Development",
+      "Front-End Development",
+      "HTML & CSS",
+      "JavaScript",
+      "Ajax",
+      "Bootstrap",
+    ],
     obtainedOn: "Oct 2023",
     url: "https://coursera.org/verify/8NS8YVRGR5PD",
   },
@@ -254,7 +258,13 @@ const certifications: Certification[] = [
       name: "Meta via Coursera",
       logo: djangoWebFramework,
     },
-    skills: ["Django", "Web Servers", "Model View Controller", "Back-End Development", "Object-Relational Mapping"],
+    skills: [
+      "Django",
+      "Web Servers",
+      "Model View Controller",
+      "Back-End Development",
+      "Object-Relational Mapping",
+    ],
     obtainedOn: "Jan 2024",
     url: "https://coursera.org/verify/4QC76KYF8VXH",
   },
@@ -264,7 +274,12 @@ const certifications: Certification[] = [
       name: "Cisco Networking Academy",
       logo: pythonEssentials1,
     },
-    skills: ["Computer Programming", "Python", "Algorithmic Thinking", "Procedural Programming"],
+    skills: [
+      "Computer Programming",
+      "Python",
+      "Algorithmic Thinking",
+      "Procedural Programming",
+    ],
     obtainedOn: "Sept 2023",
     url: "https://www.credly.com/badges/150e4072-5a5f-4be4-82a9-20fc7e607a9c/public_url",
   },
@@ -274,7 +289,13 @@ const certifications: Certification[] = [
       name: "Cisco Networking Academy",
       logo: pythonEssentials2,
     },
-    skills: ["Python", "Object-Oriented Programming", "Generators", "Modules", "Package Management"],
+    skills: [
+      "Python",
+      "Object-Oriented Programming",
+      "Generators",
+      "Modules",
+      "Package Management",
+    ],
     obtainedOn: "Oct 2023",
     url: "https://www.credly.com/badges/e00863c6-3d77-47cc-8fb2-d185499b1fb1/public_url",
   },
@@ -284,7 +305,11 @@ const certifications: Certification[] = [
       name: "University of Michigan  via Coursera",
       logo: pythonBasics,
     },
-    skills: ["Python (Programming Language)", "Data Structures", "Programming Principles"],
+    skills: [
+      "Python (Programming Language)",
+      "Data Structures",
+      "Programming Principles",
+    ],
     obtainedOn: "Apr 2023",
     url: "https://coursera.org/verify/S8WPGT8HB3MK",
   },
@@ -308,7 +333,7 @@ const certifications: Certification[] = [
     obtainedOn: "Oct 2024",
     url: "https://www.credly.com/badges/56feae9f-b2d9-4346-b4e6-ae85b22ba268/public_url",
   },
-  
+
   {
     title: "Operating Systems Basics",
     institution: {
@@ -319,18 +344,22 @@ const certifications: Certification[] = [
     obtainedOn: "Feb 2023",
     url: "https://www.credly.com/badges/809c322e-fd64-4663-a0ec-48dcf7b4a105/public_url",
   },
-  
+
   {
     title: "Object Oriented Programming in C++",
     institution: {
       name: "University of London via Coursera",
       logo: cppCrypto,
     },
-    skills: ["C++ (Programming Language)", "Object-Oriented Programming", "Programming Principles"],
+    skills: [
+      "C++ (Programming Language)",
+      "Object-Oriented Programming",
+      "Programming Principles",
+    ],
     obtainedOn: "June 2023",
     url: "https://coursera.org/verify/GXPRQWDG56EC",
   },
-  
+
   {
     title: "Introduction to Generative AI",
     institution: {
@@ -371,8 +400,7 @@ const certifications: Certification[] = [
     obtainedOn: "Sept 2023",
     url: "https://www.skills.google/public_profiles/93e7128c-3e45-4599-b1ab-d1d28d546933/badges/4956000",
   },
-  
-  
+
   {
     title: "Networking Devices and Initial Configuration",
     institution: {
@@ -389,7 +417,11 @@ const certifications: Certification[] = [
       name: "Cisco Networking Academy",
       logo: netBasics,
     },
-    skills: ["Application Layer Services", "IPV4 Addresses", "Protocol Standards"],
+    skills: [
+      "Application Layer Services",
+      "IPV4 Addresses",
+      "Protocol Standards",
+    ],
     obtainedOn: "Mar 2025",
     url: "https://www.credly.com/badges/4db1f5b9-41a9-4f5c-8d0e-b97a0440dc63/public_url",
   },
@@ -413,6 +445,6 @@ const certifications: Certification[] = [
     obtainedOn: "Apr 2025",
     url: "https://www.credly.com/badges/c65f2ea0-9a93-4048-8f78-d8dbab5e40f8/public_url",
   },
-]
+];
 
-export { technologies, expertise, services, certifications }
+export { technologies, expertise, services, certifications };
