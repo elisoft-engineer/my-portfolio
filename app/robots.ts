4ce3f602de2807080acb2d699c31133b11db2ec3
Elkana Maina <elisoft.engineer@gmail.com>
2026-09-15 10:05:18 +0300

    fix: remove private stuff

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,11 @@
-import { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
+      userAgent: "*",
+      allow: "/",
     },
-    sitemap: 'https://elkana.ellypad.com/sitemap.xml',
+    sitemap: "https://elkana.ellypad.com/sitemap.xml",
   };
 }
